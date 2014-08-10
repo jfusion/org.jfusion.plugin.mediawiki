3ece727280f73d4e4b0704450b183d14d9554778
Morten Hundevad <morten.hundevad@jfusion.rg>
2014-08-10 20:46:08 +0200

    first commit

---
+++ auth.php
@@ -0,0 +1,37 @@
+<?php namespace JFusion\Plugins\mediawiki;
+/**
+ * @category   Plugins
+ * @package    JFusion\Plugins
+ * @subpackage mediawiki
+ * @author     JFusion Team <webmaster@jfusion.org>
+ * @copyright  2008 JFusion. All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.jfusion.org
+ */
+
+use JFusion\Plugin\Plugin_Auth;
+use JFusion\User\Userinfo;
+
+/**
+ * JFusion Auth Class for mediawiki 1.1.x
+ * For detailed descriptions on these functions please check Plugin_Auth
+ *
+ * @category   Plugins
+ * @package    JFusion\Plugins
+ * @subpackage mediawiki
+ * @author     JFusion Team <webmaster@jfusion.org>
+ * @copyright  2008 JFusion. All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.jfusion.org
+ */
+class Auth extends Plugin_Auth
+{
+    /**
+     * @param Userinfo $userinfo
+     * @return string
+     */
+    function generateEncryptedPassword(Userinfo $userinfo)
+    {
+        return ':A:' . md5($userinfo->password_clear);
+    }
+}
---
+++ front.php
@@ -0,0 +1,29 @@
+<?php namespace JFusion\Plugins\mediawiki;
+/**
+ * @category   Plugins
+ * @package    JFusion\Plugins
+ * @subpackage mediawiki
+ * @author     JFusion Team <webmaster@jfusion.org>
+ * @copyright  2008 JFusion. All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.jfusion.org
+ */
+
+use JFusion\Plugin\Plugin_Front;
+
+/**
+ * JFusion Front Class for mediawiki 1.1.x
+ * For detailed descriptions on these functions please check Plugin_Front
+ *
+ * @category   Plugins
+ * @package    JFusion\Plugins
+ * @subpackage mediawiki
+ * @author     JFusion Team <webmaster@jfusion.org>
+ * @copyright  2008 JFusion. All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.jfusion.org
+ */
+class Front extends Plugin_Front
+{
+
+}
